2ff1f041f8b0dfa2b3d90e1254ec1facbf8a9f47
Ben Blake <benblake019@gmail.com>
2022-01-16 00:04:25 -0500

    Messing around with faker and benchmarking

--- mysql/lib/test.rb
+++ mysql/lib/test.rb
@@ -1,5 +1,7 @@
 require "pry"
 require "mysql2"
+require "faker"
+require "benchmark/ips"
 
 client = Mysql2::Client.new(
   host: ENV["MYSQL_HOST"],
@@ -14,17 +16,52 @@ schema_setup_sql = <<-SQL
   DROP TABLE IF EXISTS `test_table`;
   CREATE TABLE `test_table` (
     `id` bigint(20) NOT NULL AUTO_INCREMENT,
-    `name` varchar(255) DEFAULT NULL,
-    `address` varchar(255) DEFAULT NULL,
-    PRIMARY KEY (`id`)
+    `first_name` varchar(255) DEFAULT NULL,
+    `last_name` varchar(255) DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    KEY `idx_first_name` (`first_name`)
   );
 SQL
 client.query(schema_setup_sql);
 while client.next_result; end; # this is needed to execute successive commands
 
-client.query("INSERT INTO `test_table` (`name`, `address`) VALUES ('ben', '1234 main st.');")
-client.query("INSERT INTO `test_table` (`name`, `address`) VALUES ('levi', '567 yonge st.');")
-client.query("INSERT INTO `test_table` (`name`, `address`) VALUES ('heidi', '234 queen st.');")
-client.query("INSERT INTO `test_table` (`name`, `address`) VALUES ('logan', '98 king st.');")
-results = client.query("SELECT * FROM test_table;");
-results.each { |r| puts r }
+10_000.times do |i|
+  first_name = i % 100 == 0 ? "Ben" : client.escape(Faker::Name.first_name)
+  last_name =  i % 100 == 0 ? "Blake" : client.escape(Faker::Name.last_name)
+  client.query("
+    INSERT INTO `test_table` (
+      `first_name`,
+      `last_name`
+    )
+    VALUES (
+      '#{first_name}',
+      '#{last_name}'
+    );
+  ")
+end
+
+# Basic select with/without index
+Benchmark.ips do |x|
+  x.report "with index" do
+    client.query("SELECT count(*) FROM test_table WHERE first_name = 'Ben';");
+  end
+
+  x.report "without index" do
+    client.query("SELECT count(*) FROM test_table WHERE last_name = 'Blake';");
+  end
+
+  x.compare!
+end
+
+# Basic select with/without covering index
+Benchmark.ips do |x|
+  x.report "with covering index" do
+    client.query("SELECT id, first_name FROM test_table WHERE first_name = 'Ben';");
+  end
+
+  x.report "without covering index" do
+    client.query("SELECT * FROM test_table WHERE first_name = 'Ben';");
+  end
+
+  x.compare!
+end
